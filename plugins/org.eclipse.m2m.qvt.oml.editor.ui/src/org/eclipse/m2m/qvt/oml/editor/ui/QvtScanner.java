a9a4d93d92d81623894562508eb0cff0f4eaf757
aigdalov <aigdalov>
2007-08-02 17:16:37 +0000

    Bug 198713: Update list of keywords in QVT editor

--- plugins/org.eclipse.m2m.qvt.oml.editor.ui/src/org/eclipse/m2m/qvt/oml/editor/ui/QvtScanner.java
+++ plugins/org.eclipse.m2m.qvt.oml.editor.ui/src/org/eclipse/m2m/qvt/oml/editor/ui/QvtScanner.java
@@ -22,6 +22,7 @@ import org.eclipse.jface.text.rules.RuleBasedScanner;
 import org.eclipse.jface.text.rules.Token;
 import org.eclipse.jface.text.rules.WhitespaceRule;
 import org.eclipse.jface.text.rules.WordRule;
+import org.eclipse.m2m.qvt.oml.internal.cst.parser.QvtKeywords;
 import org.eclipse.swt.SWT;
 
 public class QvtScanner extends RuleBasedScanner {
@@ -30,9 +31,6 @@ public class QvtScanner extends RuleBasedScanner {
         IToken keyWord =
             new Token(
                 new TextAttribute(manager.getColor(IQvtColorConstants.TAG), null, SWT.BOLD));
-        IToken oclKeyWord =
-            new Token(
-                new TextAttribute(manager.getColor(IQvtColorConstants.OCL_TAG), null, SWT.BOLD));
         IToken defaultToken =
             new Token(
                 new TextAttribute(manager.getColor(IQvtColorConstants.DEFAULT)));
@@ -49,32 +47,16 @@ public class QvtScanner extends RuleBasedScanner {
                 return Character.isJavaIdentifierStart(character);
             }
         }, defaultToken); 
-        addKeywords(wordRule, KEYWORDS, keyWord);
-        addKeywords(wordRule, OCL_KEYWORDS, oclKeyWord);
+        
+        addKeywords(wordRule, keyWord);
         rules.add(wordRule);
         
         setRules(rules.toArray(new IRule[rules.size()]));
     }
 
-    private void addKeywords(WordRule wordRule, String[] strings, IToken keyWord) {
-        for (int i = 0; i < strings.length; i++) {
-            wordRule.addWord(strings[i], keyWord);
-        }
+    private void addKeywords(WordRule wordRule, IToken keyWord) {
+    	for (String string : QvtKeywords.KEYWORDS) {
+    		wordRule.addWord(string, keyWord);
+		}
     }
-    
-    private static final String[] KEYWORDS = {"metamodel", "main", "transformation", "query", "in", "out", "inout", "init", "end", "literal", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$ //$NON-NLS-9$ //$NON-NLS-10$
-            "mapping", "when", "var", "import", "library", "rename", "configuration", "property", "object", "while", "map", "xmap", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$ //$NON-NLS-9$ //$NON-NLS-10$ //$NON-NLS-11$ //$NON-NLS-12$
-            "modeltype", "uses", "where" }; //$NON-NLS-1$ //$NON-NLS-2$
-    private static final String[] OCL_KEYWORDS = {"implies", "and", "or", "xor", "div", "mod", "Set", "Bag", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$ //$NON-NLS-N$
-        "Sequence", "Collection", "OrderedSet", "TupleType", "Tuple", "Real", "Integer", "String", "Boolean", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-N$  
-        "iterate", "union", "collect", "select", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ 
-        "intersection", "asSequence", "asSet", "asBag", "asOrderedSet", "size", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
-        "append", "prepend", "subSequence", "at", "first", "last", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
-        "symmetricDifference", "flatten",  //$NON-NLS-1$  //$NON-NLS-2$
-        "oclIsKindOf", "oclAsType", "including", "excluding", "includes", "excludes", "oclIsTypeOf", "oclIsNew", "oclIsUndefined", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$ //$NON-NLS-9$  
-        "if", "then", "else", "endif", "not", "true", "false", "undefined", "let", "init", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$ //$NON-NLS-9$ //$NON-NLS-10$
-        "in", "allInstances",  //$NON-NLS-1$ //$NON-NLS-2$
-        "result", "self", "null", //$NON-NLS-1$  //$NON-NLS-2$ //$NON-NLS-3$
-        "reject", "exists", "forAll", "any", //$NON-NLS-1$  //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-        };  
 }
