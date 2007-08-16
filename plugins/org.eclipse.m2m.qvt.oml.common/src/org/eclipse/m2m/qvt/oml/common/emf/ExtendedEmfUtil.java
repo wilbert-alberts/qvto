ff233de75a079c31bfef01fb82e92e8c4c46bc8c
sboyko <sboyko>
2007-08-16 09:49:21 +0000

    [879]  QVTO run configuration UI should be extended to support multiple in/out models

--- plugins/org.eclipse.m2m.qvt.oml.common/src/org/eclipse/m2m/qvt/oml/common/emf/ExtendedEmfUtil.java
+++ plugins/org.eclipse.m2m.qvt.oml.common/src/org/eclipse/m2m/qvt/oml/common/emf/ExtendedEmfUtil.java
@@ -16,6 +16,7 @@ import java.util.Map;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.m2m.internal.qvt.oml.common.CommonPlugin;
 import org.eclipse.m2m.qvt.oml.common.MdaException;
 import org.eclipse.m2m.qvt.oml.common.io.CFile;
@@ -38,6 +39,25 @@ public class ExtendedEmfUtil {
 		}
     }
     
+    public static void saveModel(Resource extent, CFile file) throws MdaException {
+    	Map options = EmfUtil.DEFAULT_SAVE_OPTIONS;
+        try {
+        	URI uri = URI.createURI(file.getFileStore().toURI().toString());
+			EmfUtil.saveModel(extent, uri, options);
+		} catch (EmfException e) {
+			throw new MdaException(e);
+		} catch (IOException e) {
+			throw new MdaException(e);
+		}
+        
+        try {
+            file.refresh();
+        } 
+        catch (Exception e) {
+            throw new MdaException(CommonPlugin.getResourceString("EmfUtil.0", new Object[] {file}), e); //$NON-NLS-1$
+        }
+    }
+
     public static void saveModel(EObject eObject, CFile file) throws MdaException {
         saveModel(eObject, file, EmfUtil.DEFAULT_SAVE_OPTIONS);
     }
