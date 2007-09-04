bbb7d1cd642577794fc97ece1238c67f7a9f3341
radvorak <radvorak>
2007-09-04 07:52:01 +0000

    Adding import hyperlink support

--- plugins/org.eclipse.m2m.qvt.oml.editor.ui/src/org/eclipse/m2m/qvt/oml/editor/ui/hyperlinks/ImportHyperlinkDetector.java
+++ plugins/org.eclipse.m2m.qvt.oml.editor.ui/src/org/eclipse/m2m/qvt/oml/editor/ui/hyperlinks/ImportHyperlinkDetector.java
@@ -12,8 +12,14 @@
 package org.eclipse.m2m.qvt.oml.editor.ui.hyperlinks;
 
 
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.Region;
 import org.eclipse.jface.text.hyperlink.IHyperlink;
+import org.eclipse.m2m.qvt.oml.ast.binding.ASTBindingHelper;
+import org.eclipse.m2m.qvt.oml.common.io.CFile;
+import org.eclipse.m2m.qvt.oml.expressions.ModuleImport;
 import org.eclipse.m2m.qvt.oml.internal.cst.ModuleImportCS;
+import org.eclipse.ocl.internal.cst.CSTNode;
 import org.eclipse.ocl.internal.cst.PathNameCS;
 
 /**
@@ -22,24 +28,26 @@ import org.eclipse.ocl.internal.cst.PathNameCS;
 public class ImportHyperlinkDetector implements IHyperlinkDetectorHelper {
 
 	public IHyperlink detectHyperlink(IDetectionContext context) {
-		if (!(context.getSyntaxElement() instanceof ModuleImportCS)) {
-			return null;
+		CSTNode syntaxElement = context.getSyntaxElement();
+		ModuleImportCS moduleImportCS = null;
+		
+		if ((syntaxElement instanceof ModuleImportCS)) {
+			moduleImportCS = (ModuleImportCS) syntaxElement;
+		} else if(syntaxElement instanceof PathNameCS && syntaxElement.eContainer() instanceof ModuleImportCS) {
+			moduleImportCS = (ModuleImportCS) syntaxElement.eContainer();
 		}
-		ModuleImportCS moduleImportCS = (ModuleImportCS) context.getSyntaxElement();
-		PathNameCS pathAS = moduleImportCS.getPathNameCS();
-		if (!HyperlinkUtil.isValidElement(pathAS)) {
-			return null;
+
+		if(moduleImportCS != null) {
+			ModuleImport moduleImportAST = ASTBindingHelper.resolveASTNode(moduleImportCS, ModuleImport.class);
+			if(moduleImportAST != null && moduleImportAST.getImportedModule() != null) {
+				CFile sourceFile = ASTBindingHelper.resolveModuleFile(moduleImportAST.getImportedModule());
+				if(sourceFile != null) {
+					IRegion destRegion = new Region(0, 0);
+					return new QvtFileHyperlink(HyperlinkUtil.createRegion(moduleImportCS.getPathNameCS()), sourceFile, destRegion, destRegion);
+				}
+			}
 		}
-//		MappingModuleCS targetModuleCS = moduleImportCS.getParsedModule();
-//		if (targetModuleCS == null || !(targetModuleCS.getSource() instanceof EclipseFile)) {
-//			return null;
-//		}
-//		IFile targetFile = ((EclipseFile) targetModuleCS.getSource()).getFile();
-//		if (targetFile == null || !targetFile.exists()) {
-//			return null;
-//		}
-//		return new QvtFileHyperlink(HyperlinkUtil.createRegion(pathAS), targetFile, null, null);
+		
 		return null;
 	}
-
 }
