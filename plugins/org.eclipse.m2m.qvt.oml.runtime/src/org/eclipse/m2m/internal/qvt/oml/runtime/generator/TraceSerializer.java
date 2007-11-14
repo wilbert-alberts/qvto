af2a1e8c9ed62104977cd8f667b9985fafbe8509
sboyko <sboyko>
2007-11-14 13:47:44 +0000

    [207273] Cleanup QVTO sources

--- plugins/org.eclipse.m2m.qvt.oml.runtime/src/org/eclipse/m2m/internal/qvt/oml/runtime/generator/TraceSerializer.java
+++ plugins/org.eclipse.m2m.qvt.oml.runtime/src/org/eclipse/m2m/internal/qvt/oml/runtime/generator/TraceSerializer.java
@@ -21,7 +21,10 @@ import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EValidator;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xmi.XMLResource;
 import org.eclipse.m2m.qvt.oml.ast.parser.QvtOperationalTypesUtil;
 import org.eclipse.m2m.qvt.oml.common.MdaException;
@@ -63,6 +66,10 @@ public class TraceSerializer {
 	    attributes.put(IMarker.MESSAGE, NLS.bind("Trace contains unbounded object of type ''{0}''", //$NON-NLS-1$
 	    		QvtOperationalTypesUtil.getTypeFullName(eObj.eClass())));
 	    attributes.put(IMarker.SEVERITY, IMarker.SEVERITY_WARNING);
+	    URI uri = EcoreUtil.getURI(eObj);
+	    if (uri != null) {
+	    	attributes.put(EValidator.URI_ATTRIBUTE, uri.fragment());
+	    }
 	    try {
 	        IMarker marker = iresource.createMarker(QvtCompiler.PROBLEM_MARKER);
 	        marker.setAttributes(attributes);
