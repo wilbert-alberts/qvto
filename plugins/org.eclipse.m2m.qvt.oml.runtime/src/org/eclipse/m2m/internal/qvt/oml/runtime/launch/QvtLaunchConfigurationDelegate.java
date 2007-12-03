e5bb526f1eba800a683744d6ae7c0f972adc741f
radvorak <radvorak>
2007-12-03 13:37:43 +0000

    [211731] Support interrubtible QVTO evaluation in run mode - merging from QVTO_M1_Maintenance branch

--- plugins/org.eclipse.m2m.qvt.oml.runtime/src/org/eclipse/m2m/internal/qvt/oml/runtime/launch/QvtLaunchConfigurationDelegate.java
+++ plugins/org.eclipse.m2m.qvt.oml.runtime/src/org/eclipse/m2m/internal/qvt/oml/runtime/launch/QvtLaunchConfigurationDelegate.java
@@ -19,14 +19,19 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.m2m.internal.qvt.oml.runtime.project.QvtInterpretedTransformation;
 import org.eclipse.m2m.internal.qvt.oml.runtime.project.QvtTransformation;
+import org.eclipse.m2m.qvt.oml.QvtPlugin;
 import org.eclipse.m2m.qvt.oml.ast.environment.QvtOperationalStdLibrary;
 import org.eclipse.m2m.qvt.oml.common.launch.ShallowProcess;
 import org.eclipse.m2m.qvt.oml.common.launch.StreamsProxy;
 import org.eclipse.m2m.qvt.oml.compiler.QvtCompiler;
+import org.eclipse.m2m.qvt.oml.internal.ast.evaluator.EvaluationContextProperties;
+import org.eclipse.m2m.qvt.oml.internal.ast.evaluator.EvaluationMonitor;
+import org.eclipse.m2m.qvt.oml.internal.ast.evaluator.QvtRuntimeException;
 import org.eclipse.m2m.qvt.oml.library.Context;
 import org.eclipse.m2m.qvt.oml.library.IContext;
 
@@ -35,12 +40,13 @@ public class QvtLaunchConfigurationDelegate extends QvtLaunchConfigurationDelega
 	public QvtLaunchConfigurationDelegate() {
 	}
 	
+	// FIXME - do refactoring of this area 
 	public void launch(final ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
         
 		try {
             final QvtTransformation qvtTransformation = new QvtInterpretedTransformation(getQvtModule(configuration));
-                        
-            
+            final EvaluationMonitor execMonitor = createMonitor();
+                                    
             StreamsProxy streamsProxy = new StreamsProxy();
             final PrintWriter printWriter = new PrintWriter(streamsProxy.getOutputWriter());   
             ShallowProcess.IRunnable r = new ShallowProcess.IRunnable() {
@@ -53,19 +59,67 @@ public class QvtLaunchConfigurationDelegate extends QvtLaunchConfigurationDelega
                 	
                 	IContext context = new Context(QvtLaunchUtil.getConfiguration(configuration));
                     context.put(QvtOperationalStdLibrary.OUT_PRINT_WRITER, printWriter);
-
+                    context.put(EvaluationContextProperties.MONITOR, execMonitor);
+                    
                 	QvtLaunchConfigurationDelegateBase.doLaunch(qvtTransformation, configuration, context);
                 }
             };
             
-            r = getSafeRunnable(qvtTransformation, r);                        
-            ShallowProcess process = new ShallowProcess(launch, r);
+            
+            r = getSafeRunnable(qvtTransformation, r);       
+            final ShallowProcess process = new ShallowProcess(launch, r) {
+            	@Override
+            	public void terminate() throws DebugException {
+            		execMonitor.cancel();
+            		super.terminate();
+            	}
+
+            	@Override
+            	public boolean isTerminated() {            	
+            		return execMonitor.isCanceled();
+            	}
+            	
+            	@Override
+            	public boolean canTerminate() {
+            		return !isTerminated();
+            	}
+            };
+            
             process.setStreamsProxy(streamsProxy);
-            process.run();        
+            
+            Thread processThread = new Thread(new Runnable() {
+            	public void run() {
+                    try {
+						process.run();
+					} catch (Exception e) {
+						if(e instanceof QvtRuntimeException) {
+							((QvtRuntimeException)e).printQvtStackTrace(new PrintWriter(System.err));
+						} else {
+							QvtPlugin.log(e);
+						}
+					}
+            	}
+            }, "QVT Run");
+            
+            processThread.start();
 		}
 		catch(Exception e) {
 			throw new CoreException(org.eclipse.m2m.internal.qvt.oml.runtime.util.MiscUtil.makeErrorStatus(e));
 		}
+	}
+
+	private EvaluationMonitor createMonitor() {
+		return new EvaluationMonitor() {
+			boolean fIsCanceled = false;
+			
+			public void cancel() {
+				fIsCanceled = true;
+			}
+			
+			public boolean isCanceled() {
+				return fIsCanceled;
+			}
+		};
 	}
     
     
