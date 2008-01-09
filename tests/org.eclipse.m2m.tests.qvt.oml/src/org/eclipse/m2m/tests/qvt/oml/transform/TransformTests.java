121a5264e95eccb953189095e7e1e4d98678fd3b
aigdalov <aigdalov>
2008-01-09 09:28:22 +0000

    [214718] Cannot refer to a metamodel by a platform URI

--- tests/org.eclipse.m2m.tests.qvt.oml/src/org/eclipse/m2m/tests/qvt/oml/transform/TransformTests.java
+++ tests/org.eclipse.m2m.tests.qvt.oml/src/org/eclipse/m2m/tests/qvt/oml/transform/TransformTests.java
@@ -80,6 +80,7 @@ public class TransformTests {
     
     public static ModelTestData[] createTestData() {
     	return new ModelTestData[] {
+    	new FileToFileData("bug214718"), //$NON-NLS-1$        
     	new FileToFileData("oclannotation", "in.ecore", "expected.xmi").includeMetamodelFile("metamodel.ecore"), //$NON-NLS-1$    			
     	new FileToFileData("importedvirtuals"), //$NON-NLS-1$    	
     	new FileToFileData("ocl_test"), //$NON-NLS-1$    			
