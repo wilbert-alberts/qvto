c632b270c1d516e5a70f3423e579c307973146ce
radvorak <radvorak>
2008-01-10 23:59:18 +0000

    [214821] while loop expression has invalid syntax

--- plugins/org.eclipse.m2m.qvt.oml/src/org/eclipse/m2m/qvt/oml/library/AbstractLateResolveTask.java
+++ plugins/org.eclipse.m2m.qvt.oml/src/org/eclipse/m2m/qvt/oml/library/AbstractLateResolveTask.java
@@ -15,6 +15,7 @@ import org.eclipse.m2m.qvt.oml.ast.environment.QvtOperationalEvaluationEnv;
 import org.eclipse.m2m.qvt.oml.ast.parser.QvtOperationalUtil;
 import org.eclipse.m2m.qvt.oml.expressions.AssignExp;
 import org.eclipse.m2m.qvt.oml.expressions.ResolveExp;
+import org.eclipse.m2m.qvt.oml.internal.ast.evaluator.DeferredAssignmentListener;
 import org.eclipse.m2m.qvt.oml.internal.ast.evaluator.QvtOperationalEvaluationVisitor;
 import org.eclipse.m2m.qvt.oml.library.QvtResolveUtil.SavedSourceObjectHolder;
 
@@ -28,10 +29,11 @@ public abstract class AbstractLateResolveTask implements Runnable, SavedSourceOb
     private final QvtOperationalEvaluationVisitor myVisitor;
     private final QvtOperationalEvaluationEnv myEvalEnv;
     private final EObjectEStructuralFeaturePair myLvalue;
-    private Object mySourceObj;    
+    private Object mySourceObj;
+	private final DeferredAssignmentListener myListener;    
 
     public AbstractLateResolveTask(ResolveExp resolveExp, EObjectEStructuralFeaturePair lvalue, QvtOperationalEvaluationVisitor visitor,
-            QvtOperationalEvaluationEnv evalEnv) {
+            QvtOperationalEvaluationEnv evalEnv, DeferredAssignmentListener deferredListener) {
                 myResolveExp = resolveExp;
                 myLvalue = lvalue;
                 myVisitor = visitor;
@@ -39,7 +41,9 @@ public abstract class AbstractLateResolveTask implements Runnable, SavedSourceOb
                 
                 if(myResolveExp.getSource() != null) {                	
                 	mySourceObj = myResolveExp.getSource().accept(visitor);
-                }                  
+                }             
+                
+                myListener = deferredListener;
     }
     
     public void run() {
@@ -53,6 +57,10 @@ public abstract class AbstractLateResolveTask implements Runnable, SavedSourceOb
             if(assignExp != null) {
             	myEvalEnv.callSetter(myLvalue.getEObject(), myLvalue.getEStructuralFeature(), 
             			resolveResult, QvtOperationalUtil.isUndefined(resolveResult), assignExp.isIsReset());
+            	
+            	if(myListener != null) {
+            		myListener.notifyAfterDeferredAssign(assignExp, resolveResult);
+            	}
             } else {
             	// there was not appropriate assignment to receive the value  
             }
