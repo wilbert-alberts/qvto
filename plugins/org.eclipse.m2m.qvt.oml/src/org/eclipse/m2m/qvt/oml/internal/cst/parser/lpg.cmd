51918469223dd6b082cba2d54173846e36b43275
sboyko <sboyko>
2008-03-12 12:01:14 +0000

    [222393]  Refine CST parser

--- plugins/org.eclipse.m2m.qvt.oml/src/org/eclipse/m2m/qvt/oml/internal/cst/parser/lpg.cmd
+++
@@ -1,8 +0,0 @@
-set HOME=N:\stuff\LPG\lpgdistribution
-set LPG_INCLUDE=%HOME%\include;.;
-set LPG_TEMPLATE=%HOME%\templates;
-rem %HOME%\lpgexe\lpg.exe -list QvtOpLPGParser.g
-rem %HOME%\lpgexe\lpg.exe -soft-keywords -backtrack -list QvtOpLPGParser.backtrack.g
-%HOME%\lpgexe\lpg.exe -backtrack -list QvtOpLPGParser.backtrack.g
-
-pause
