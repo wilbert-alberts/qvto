e45ad6dcda3ff21ced63b69cd58107f97d7bf36b
radvorak <radvorak>
2008-04-03 23:55:51 +0000

    [225664] Enhance the operation set defined on Element type

--- plugins/org.eclipse.m2m.qvt.oml.common/src/org/eclipse/m2m/internal/qvt/oml/common/launch/InMemoryLaunchUtils.java
+++ plugins/org.eclipse.m2m.qvt.oml.common/src/org/eclipse/m2m/internal/qvt/oml/common/launch/InMemoryLaunchUtils.java
@@ -12,6 +12,7 @@
 package org.eclipse.m2m.internal.qvt.oml.common.launch;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -23,16 +24,21 @@ public class InMemoryLaunchUtils {
     private InMemoryLaunchUtils() {}
     
     public static Object getAttribute(ILaunchConfiguration config, String name) throws CoreException {
-        List value = config.getAttribute(name, Collections.EMPTY_LIST);
-        if(value.isEmpty()) {
+        Object value = config.getAttributes().get(name);
+        if(value instanceof Collection == false) {
+        	return value;
+        }
+        
+        Collection<?> listValue = (Collection<?>) value;
+        if(listValue.isEmpty()) {
             return null;
         }
         
-        if(value.size() != 1) {
+        if(listValue.size() != 1) {
             throw new IllegalArgumentException("Invalid value for " + name + ": " + value); //$NON-NLS-1$ //$NON-NLS-2$
         }
         
-        return value.get(0);
+        return listValue.iterator().next();
     }
     
     public static void setAttribute(ILaunchConfigurationWorkingCopy config, String name, Object value) {
