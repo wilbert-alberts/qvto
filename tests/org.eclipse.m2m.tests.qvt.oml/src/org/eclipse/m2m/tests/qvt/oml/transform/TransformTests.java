7cc027b6daf7f8483efdbca870735a25d83c2d67
radvorak <radvorak>
2008-04-29 15:58:55 +0000

    [219415] Support multiple results in mapping operations - forgotten commit

--- tests/org.eclipse.m2m.tests.qvt.oml/src/org/eclipse/m2m/tests/qvt/oml/transform/TransformTests.java
+++ tests/org.eclipse.m2m.tests.qvt.oml/src/org/eclipse/m2m/tests/qvt/oml/transform/TransformTests.java
@@ -81,7 +81,8 @@ public class TransformTests {
     }
 
     public static ModelTestData[] createTestData() {
-        return new ModelTestData[] {
+        return new ModelTestData[] {        		
+        		new FileToFileData("collectionMappingResult"), //$NON-NLS-1$
         		new FileToFileData("intermediateprop_import"), //$NON-NLS-1$ 
                 new FileToFileData("intermediateprop_resolve", "in.simpleuml", "expected.simpleuml"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
                 new FileToFileData("intermediateprop_trace", "in.simpleuml", "expected.simpleuml"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
