3742de27729dc76e6b3f0f05aec3519da87f6b93
aigdalov <aigdalov>
2008-08-26 16:28:44 +0000

    [244701] QVTO: No way to define a non-initialized variable [4479] QVTO: parser errors on variable declaration if initialization value is omitted and var type differs from Boolean

--- tests/org.eclipse.m2m.tests.qvt.oml/src/org/eclipse/m2m/tests/qvt/oml/transform/TransformTests.java
+++ tests/org.eclipse.m2m.tests.qvt.oml/src/org/eclipse/m2m/tests/qvt/oml/transform/TransformTests.java
@@ -81,7 +81,8 @@ public class TransformTests {
     }
 
     public static ModelTestData[] createTestData() {
-        return new ModelTestData[] {
+        return new ModelTestData[] {        		        		
+                new FileToFileData("bug244701"), //$NON-NLS-1$
         		new FileToFileData("virtualPredefinedTypeOpers"), //$NON-NLS-1$        		
         		new FileToFileData("blackboxlib_237781"), //$NON-NLS-1$        		
                 new FileToFileData("bug233984"), //$NON-NLS-1$
