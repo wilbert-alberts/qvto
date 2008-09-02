228fc1c19a1e6730a36d28ce9b8f52e5c7c4a871
radvorak <radvorak>
2008-09-02 20:36:00 +0000

    [236128] Initial commit of the UMLX project contribution.

---
+++ plugins/org.eclipse.m2m.qvt.oml.emof.qvtoperational/src/org/eclipse/m2m/qvt/oml/emof/QVTOperational/impl/ConstructorBodyImpl.java
@@ -0,0 +1,43 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id: ConstructorBodyImpl.java,v 1.1 2008/09/02 20:36:01 radvorak Exp $
+ */
+package org.eclipse.m2m.qvt.oml.emof.QVTOperational.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.m2m.qvt.oml.emof.QVTOperational.ConstructorBody;
+import org.eclipse.m2m.qvt.oml.emof.QVTOperational.QVTOperationalPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Constructor Body</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class ConstructorBodyImpl extends OperationBodyImpl implements ConstructorBody {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ConstructorBodyImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return QVTOperationalPackage.Literals.CONSTRUCTOR_BODY;
+	}
+
+} //ConstructorBodyImpl
---
+++ plugins/org.eclipse.m2m.qvt.oml.emof.qvtoperational/src/org/eclipse/m2m/qvt/oml/emof/QVTOperational/impl/EntryOperationImpl.java
@@ -0,0 +1,43 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id: EntryOperationImpl.java,v 1.1 2008/09/02 20:36:01 radvorak Exp $
+ */
+package org.eclipse.m2m.qvt.oml.emof.QVTOperational.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.m2m.qvt.oml.emof.QVTOperational.EntryOperation;
+import org.eclipse.m2m.qvt.oml.emof.QVTOperational.QVTOperationalPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Entry Operation</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class EntryOperationImpl extends ImperativeOperationImpl implements EntryOperation {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected EntryOperationImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return QVTOperationalPackage.Literals.ENTRY_OPERATION;
+	}
+
+} //EntryOperationImpl
