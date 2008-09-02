374cf110a5f1d7891609c5be041c3a1117dbc569
radvorak <radvorak>
2008-09-02 20:02:40 +0000

    [236128] Initial commit of the UMLX project contribution.

---
+++ plugins/org.eclipse.m2m.qvt.oml.ecore.imperativeocl.editor/src/org/eclipse/m2m/qvt/oml/ecore/ImperativeOCL/presentation/ImperativeOCLEditorPlugin.java
@@ -0,0 +1,112 @@
+/**
+ * <copyright>
+ * Copyright (c) 2008 Open Canarias S.L. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     A. Sanchez-Barbudo  - initial API and implementation
+ * </copyright>
+ *
+ * $Id: ImperativeOCLEditorPlugin.java,v 1.1 2008/09/02 20:02:40 radvorak Exp $
+ */
+package org.eclipse.m2m.qvt.oml.ecore.ImperativeOCL.presentation;
+
+import org.eclipse.emf.common.EMFPlugin;
+
+import org.eclipse.emf.common.ui.EclipseUIPlugin;
+
+import org.eclipse.emf.common.util.ResourceLocator;
+
+import org.eclipse.emf.ecore.provider.EcoreEditPlugin;
+
+import org.eclipse.ocl.ecore.provider.OCLEcoreEditPlugin;
+
+import org.eclipse.ocl.edit.internal.OCLEditPlugin;
+
+/**
+ * This is the central singleton for the ImperativeOCL editor plugin.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public final class ImperativeOCLEditorPlugin extends EMFPlugin {
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final ImperativeOCLEditorPlugin INSTANCE = new ImperativeOCLEditorPlugin();
+	
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private static Implementation plugin;
+
+	/**
+	 * Create the instance.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ImperativeOCLEditorPlugin() {
+		super
+			(new ResourceLocator [] {
+				EcoreEditPlugin.INSTANCE,
+				OCLEditPlugin.INSTANCE,
+				OCLEcoreEditPlugin.INSTANCE,
+			});
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	@Override
+	public ResourceLocator getPluginResourceLocator() {
+		return plugin;
+	}
+	
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	public static Implementation getPlugin() {
+		return plugin;
+	}
+	
+	/**
+	 * The actual implementation of the Eclipse <b>Plugin</b>.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static class Implementation extends EclipseUIPlugin {
+		/**
+		 * Creates an instance.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @generated
+		 */
+		public Implementation() {
+			super();
+	
+			// Remember the static instance.
+			//
+			plugin = this;
+		}
+	}
+
+}
