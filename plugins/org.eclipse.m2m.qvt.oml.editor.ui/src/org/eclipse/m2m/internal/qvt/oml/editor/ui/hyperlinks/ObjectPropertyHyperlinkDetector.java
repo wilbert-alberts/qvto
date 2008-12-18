f663f3666e743d256ceee1aa0622ae8eacdba09e
radvorak <radvorak>
2008-12-18 15:18:15 +0000

    [259121] Eliminate non-std LocalProperty, ConfigProperty from the AST metamodel

--- plugins/org.eclipse.m2m.qvt.oml.editor.ui/src/org/eclipse/m2m/internal/qvt/oml/editor/ui/hyperlinks/ObjectPropertyHyperlinkDetector.java
+++ plugins/org.eclipse.m2m.qvt.oml.editor.ui/src/org/eclipse/m2m/internal/qvt/oml/editor/ui/hyperlinks/ObjectPropertyHyperlinkDetector.java
@@ -16,14 +16,15 @@ import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.hyperlink.IHyperlink;
 import org.eclipse.m2m.internal.qvt.oml.ast.binding.ASTBindingHelper;
-import org.eclipse.m2m.internal.qvt.oml.ast.parser.QvtOperationalParserUtil;
+import org.eclipse.m2m.internal.qvt.oml.ast.binding.ASTSyntheticNode;
+import org.eclipse.m2m.internal.qvt.oml.ast.binding.ASTSyntheticNodeAccess;
 import org.eclipse.m2m.internal.qvt.oml.common.io.CFile;
+import org.eclipse.m2m.internal.qvt.oml.cst.CSTPackage;
 import org.eclipse.m2m.internal.qvt.oml.cst.LocalPropertyCS;
 import org.eclipse.m2m.internal.qvt.oml.cst.ModulePropertyCS;
+import org.eclipse.m2m.internal.qvt.oml.cst.temp.ScopedNameCS;
 import org.eclipse.m2m.internal.qvt.oml.editor.ui.CSTHelper;
-import org.eclipse.m2m.internal.qvt.oml.expressions.LocalProperty;
-import org.eclipse.m2m.internal.qvt.oml.expressions.Module;
-import org.eclipse.m2m.internal.qvt.oml.expressions.Property;
+import org.eclipse.m2m.internal.qvt.oml.expressions.ContextualProperty;
 import org.eclipse.ocl.cst.CSTNode;
 import org.eclipse.ocl.cst.FeatureCallExpCS;
 import org.eclipse.ocl.cst.SimpleNameCS;
@@ -39,59 +40,57 @@ public class ObjectPropertyHyperlinkDetector implements IHyperlinkDetectorHelper
 		CSTNode syntaxElement = context.getSyntaxElement();
 		EStructuralFeature referredFeature = findDefinition(syntaxElement);
 			
-		if(referredFeature != null) {			
-			Property prop = getASTProperty(referredFeature);
-			if(prop != null) {
-				ModulePropertyCS propertyCS = ASTBindingHelper.resolveCSTNode(prop, ModulePropertyCS.class);
-				if(propertyCS != null) {
-					CFile sourceFile = CSTHelper.getSourceFile(propertyCS);
-					CSTNode destNodeCS = (propertyCS.getSimpleNameCS() != null) ? propertyCS.getSimpleNameCS() : propertyCS;
-					IRegion destRegion = HyperlinkUtil.createRegion(destNodeCS);						
-					return new QvtFileHyperlink(HyperlinkUtil.createRegion(syntaxElement), sourceFile, destRegion, destRegion);	
-				}
+		if(referredFeature != null) {
+			ModulePropertyCS propertyCS = findDefinitionCS(referredFeature);
+			if(propertyCS != null) {
+				CFile sourceFile = CSTHelper.getSourceFile(propertyCS);
+				CSTNode destNodeCS = (propertyCS.getSimpleNameCS() != null) ? propertyCS.getSimpleNameCS() : propertyCS;
+				IRegion destRegion = HyperlinkUtil.createRegion(destNodeCS);						
+				return new QvtFileHyperlink(HyperlinkUtil.createRegion(syntaxElement), sourceFile, destRegion, destRegion);	
 			}
 			
 			return new MetamodelElementHyperlink(HyperlinkUtil.createRegion(syntaxElement), referredFeature);
 		}
 		
 		return null;
-	}	
+	}
+	
+	public static ModulePropertyCS findDefinitionCS(EStructuralFeature referencedFeature) {
+		ModulePropertyCS referencedDefinitionCS;
+		ASTSyntheticNode referencedDefinitionAST = ASTSyntheticNodeAccess.getASTNode(referencedFeature);
+		if(referencedDefinitionAST != null) {
+			referencedDefinitionCS = ASTSyntheticNodeAccess.getCST(referencedDefinitionAST, ModulePropertyCS.class);
+		} else {
+			referencedDefinitionCS = ASTBindingHelper.resolveCSTNode(referencedFeature, ModulePropertyCS.class);
+		}
+		return referencedDefinitionCS;
+	}
 	
 	public static EStructuralFeature findDefinition(CSTNode syntaxElement) {
 		if (syntaxElement instanceof SimpleNameCS) {
 			EStructuralFeature result = findDefinition((SimpleNameCS) syntaxElement);
 			if(result instanceof TupleType) {
 				return null;
-			}				
+			}
 			
 			return result;
+		} else if(syntaxElement instanceof ScopedNameCS) {
+			if(syntaxElement.eContainingFeature() == CSTPackage.eINSTANCE.getContextualPropertyCS_ScopedNameCS()) {
+				return ASTBindingHelper.resolveASTNode((CSTNode)syntaxElement.eContainer(), ContextualProperty.class);
+			}			
 		}
 		
 		return null;
 	}
-
-	
-	public static Property getASTProperty(EStructuralFeature feature) {
-		if(feature instanceof Property) {
-			return (Property)feature; // includes intermediate property
-		}
-		EStructuralFeature originalFeature = feature;
-		if (originalFeature.eContainer() instanceof Module) {
-			Property localProp = QvtOperationalParserUtil.getLocalPropertyAST(feature);
-			if(localProp != null) {
-				return localProp;
-			}
-		}
 		
-		return null;
-	}
-	
 	@SuppressWarnings("unchecked")
 	private static EStructuralFeature findDefinition(SimpleNameCS nameCS) {
-		if(nameCS.eContainer() instanceof FeatureCallExpCS) {				
-			FeatureCallExpCS featureCallExpCS = (FeatureCallExpCS) nameCS.eContainer();
-			ASTNode featureASTNode = ASTBindingHelper.resolveASTNode(featureCallExpCS);
-			
+		if(nameCS.eContainingFeature() == CSTPackage.eINSTANCE.getModulePropertyCS_SimpleNameCS()) {
+			return (EStructuralFeature)((CSTNode)nameCS.eContainer()).getAst();
+		} 
+		
+		if(nameCS.eContainer() instanceof FeatureCallExpCS) {
+			ASTNode featureASTNode = directGetASTNode(nameCS);			
 			if(featureASTNode instanceof PropertyCallExp) {
 				PropertyCallExp<EClassifier, EStructuralFeature> propertyCallExp = (PropertyCallExp<EClassifier, EStructuralFeature>)featureASTNode;
 				return propertyCallExp.getReferredProperty();												
@@ -107,10 +106,17 @@ public class ObjectPropertyHyperlinkDetector implements IHyperlinkDetectorHelper
 				}
 			}
 		} else if(nameCS.eContainer() instanceof LocalPropertyCS) {
-			LocalPropertyCS localPropertyCS = (LocalPropertyCS) nameCS.eContainer();
-			return ASTBindingHelper.resolveASTNode(localPropertyCS, LocalProperty.class);			
+			LocalPropertyCS localPropertyCS = (LocalPropertyCS) nameCS.eContainer(); 
+			if(localPropertyCS.getAst() instanceof EStructuralFeature) {
+				return (EStructuralFeature) localPropertyCS.getAst();
+			}
 		}
-		
+				
 		return null;	
+	}
+	
+	private static ASTNode directGetASTNode(CSTNode cstNode) {
+		Object ast = cstNode.getAst();		
+		return ast instanceof ASTNode ? (ASTNode) ast : null;
 	}
 }
