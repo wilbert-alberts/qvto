045d553e5300ad7bdf02066a17f766cf4eea3087
aigdalov <aigdalov>
2009-01-19 15:21:46 +0000

    [261024] Allow statements be used in expressions

--- tests/org.eclipse.m2m.tests.qvt.oml/src/org/eclipse/m2m/tests/qvt/oml/transform/TransformTests.java
+++ tests/org.eclipse.m2m.tests.qvt.oml/src/org/eclipse/m2m/tests/qvt/oml/transform/TransformTests.java
@@ -78,6 +78,7 @@ public class TransformTests {
     
     public static ModelTestData[] createTestData() {
         return new ModelTestData[] {
+        		new FileToFileData("chainedAssignments_261024"), //$NON-NLS-1$
         		new FileToFileData("_while_261024"), //$NON-NLS-1$
         		new FileToFileData("varInitExpWithResult_260985"), //$NON-NLS-1$
         		new FileToFileData("stdlibList"), //$NON-NLS-1$
