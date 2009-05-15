45538093c94c344b8b69809ec896579112745721
radvorak <radvorak>
2009-05-15 12:01:11 +0000

    [276476] Eliminate non-externalized strings in generated java models

--- plugins/org.eclipse.m2m.qvt.oml/_generated/org/eclipse/m2m/internal/qvt/oml/trace/TracePackage.java
+++ plugins/org.eclipse.m2m.qvt.oml/_generated/org/eclipse/m2m/internal/qvt/oml/trace/TracePackage.java
@@ -9,7 +9,7 @@
  * Contributors:
  *     Borland Software Corporation - initial API and implementation
  *
- * $Id: TracePackage.java,v 1.2 2008/12/18 15:18:10 radvorak Exp $
+ * $Id: TracePackage.java,v 1.3 2009/05/15 12:01:11 radvorak Exp $
  */
 package org.eclipse.m2m.internal.qvt.oml.trace;
 
@@ -40,7 +40,7 @@ public interface TracePackage extends EPackage {
      * <!-- end-user-doc -->
 	 * @generated
 	 */
-    String copyright = "Copyright (c) 2007 Borland Software Corporation\r\n\r\nAll rights reserved. This program and the accompanying materials\r\nare made available under the terms of the Eclipse Public License v1.0\r\nwhich accompanies this distribution, and is available at\r\nhttp://www.eclipse.org/legal/epl-v10.html\r\n  \r\nContributors:\r\n    Borland Software Corporation - initial API and implementation";
+    String copyright = "Copyright (c) 2007 Borland Software Corporation\r\n\r\nAll rights reserved. This program and the accompanying materials\r\nare made available under the terms of the Eclipse Public License v1.0\r\nwhich accompanies this distribution, and is available at\r\nhttp://www.eclipse.org/legal/epl-v10.html\r\n  \r\nContributors:\r\n    Borland Software Corporation - initial API and implementation"; //$NON-NLS-1$
 
     /**
 	 * The package name.
@@ -48,7 +48,7 @@ public interface TracePackage extends EPackage {
      * <!-- end-user-doc -->
 	 * @generated
 	 */
-    String eNAME = "trace";
+    String eNAME = "trace"; //$NON-NLS-1$
 
     /**
 	 * The package namespace URI.
@@ -56,7 +56,7 @@ public interface TracePackage extends EPackage {
      * <!-- end-user-doc -->
 	 * @generated
 	 */
-    String eNS_URI = "http:///www.eclipse.org/m2m/qvt/operational/trace.ecore";
+    String eNS_URI = "http:///www.eclipse.org/m2m/qvt/operational/trace.ecore"; //$NON-NLS-1$
 
     /**
 	 * The package namespace name.
@@ -64,7 +64,7 @@ public interface TracePackage extends EPackage {
      * <!-- end-user-doc -->
 	 * @generated
 	 */
-    String eNS_PREFIX = "trace";
+    String eNS_PREFIX = "trace"; //$NON-NLS-1$
 
     /**
 	 * The singleton instance of the package.
