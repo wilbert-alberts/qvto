4a41d005a365179609e869d7e0306bc11537b89d
radvorak <radvorak>
2009-10-07 13:26:27 +0000

    [291594] Refactore usage of meta-model registry in QVTO Compiler

--- plugins/org.eclipse.m2m.qvt.oml/src/org/eclipse/m2m/internal/qvt/oml/ast/parser/MetamodelResolutionHelper.java
+++ plugins/org.eclipse.m2m.qvt.oml/src/org/eclipse/m2m/internal/qvt/oml/ast/parser/MetamodelResolutionHelper.java
@@ -15,27 +15,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EClassifier;
-import org.eclipse.emf.ecore.EEnumLiteral;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EOperation;
 import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.EParameter;
-import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.m2m.internal.qvt.oml.ast.env.QvtOperationalEnv;
-import org.eclipse.m2m.internal.qvt.oml.ast.env.QvtOperationalFileEnv;
-import org.eclipse.m2m.internal.qvt.oml.common.resourcesetprovider.ResourceSetProviderRegistry.ResourceSetResourceSetProviderPair;
 import org.eclipse.m2m.internal.qvt.oml.compiler.QvtCompilerOptions;
 import org.eclipse.m2m.internal.qvt.oml.emf.util.EmfException;
 import org.eclipse.m2m.internal.qvt.oml.emf.util.mmregistry.IMetamodelDesc;
 import org.eclipse.m2m.internal.qvt.oml.emf.util.mmregistry.MetamodelRegistry;
-import org.eclipse.ocl.Environment.Internal;
-import org.eclipse.ocl.ecore.CallOperationAction;
-import org.eclipse.ocl.ecore.Constraint;
-import org.eclipse.ocl.ecore.SendSignalAction;
 
 /**
  * FIXME - this class extracts the logic of resolving workspace located metamodels, 
@@ -55,84 +41,40 @@ class MetamodelResolutionHelper {
 			MetamodelRegistry metamodelRegistry, QvtCompilerOptions options) {
         List<EPackage> metamodels = new ArrayList<EPackage>(1);
 		try {
-		    IMetamodelDesc[] desc = null;
-		    Internal<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env = qvtEnv;
-		    while (env != null && metamodelUri != null) {
-		        if (env instanceof QvtOperationalFileEnv) {
-		            QvtOperationalFileEnv fileEnv = (QvtOperationalFileEnv) env;
-		            ResourceSet resourceSet = fileEnv.getKernel().getMetamodelResourceSet();
-		            ResourceSetResourceSetProviderPair pair = null;
-		            if (resourceSet == null) {
-	                    URI uri = fileEnv.getFile();
-	                    // FIXME - refactore CFile out of here
-						/* comment Eclipse dependency out
-	                    IFile iFileURI = URIUtils.getFile(uri);
-						if(iFileURI != null) {
-							CFile cFile = new EclipseFile(iFileURI);
-		                    pair = ResourceSetProviderRegistry.getResourceSetResourceSetProviderPair(cFile);
-		                    if (pair != null) {
-		                        ResourceSet providedResourceSet = pair.getResourceSet();
-		                        URI mmURI = URI.createURI(metamodelUri);
-	                            if (EmfUtil.isUriMapped(providedResourceSet, mmURI)) {
-	                                resourceSet = providedResourceSet;
-		                        }
-		                    }
-						}*/
-		            }
-		            if (resourceSet != null) {
-		                IMetamodelDesc metamodelDesc = MetamodelRegistry.createUndeclaredMetamodel(metamodelUri, resourceSet);
-		                if (metamodelDesc != null) {
-	                        desc = new IMetamodelDesc[] { metamodelDesc };
-		                }
-		                if (pair != null) {
-		                    pair.getResourceSetProvider().dispose(resourceSet);
-		                }
-		            }
-                    break;
-		        }
-                env = env.getInternalParent();
-		    }
-		    
-		    if (desc == null) {
-	            MetamodelRegistry registry = metamodelRegistry;
-	            
-	            if (path.isEmpty()) {
-	                desc = new IMetamodelDesc[] { registry.getMetamodelDesc(metamodelUri, 
-	                		options != null && options.getMetamodelResourceSet() != null ? options.getMetamodelResourceSet() : resolutionRS) };
-	            }
-	            else {
-	                desc = registry.getMetamodelDesc(path);
-	            }
-		    }
-
-			for(IMetamodelDesc nextDesc : desc) {
-				if(nextDesc.getModel() == null) {
-					continue;
-				}
-				
-	        	EPackage model = nextDesc.getModel();
-	            // register metamodel for EClassifier lookup
-	        	if (model.getNsURI() == null) {
-					while (true) {
-						if (model.getESuperPackage() == null) {
-							break;
-						}
-						model = model.getESuperPackage();
+		    IMetamodelDesc[] desc;
+            MetamodelRegistry registry = metamodelRegistry;
+            
+            if (path.isEmpty()) {
+                desc = new IMetamodelDesc[] { registry.getMetamodelDesc(metamodelUri) };
+            }
+            else {
+                desc = registry.getMetamodelDesc(path);
+            }
+                        
+            if(desc != null) {
+				for(IMetamodelDesc nextDesc : desc) {
+					if(nextDesc.getModel() == null) {
+						continue;
 					}
-	        	}
-	        	
-	        	if(nextDesc.getLoadStatus().getSeverity() < IStatus.ERROR) {
-	        		metamodels.add(model);	        	
-	        		qvtEnv.getEPackageRegistry().put(model.getNsURI(), model);
-	        	}
-	            break;
-/*
-	            EPackage[] all = EmfMmUtil.getRegisterableModels(model, true);            
-	            for (EPackage pack : all) {
-//		            getEPackageRegistry().put(pack.getNsURI(), pack);
-	            }
-*/
-	        }			
+					
+		        	EPackage model = nextDesc.getModel();
+		            // register metamodel for EClassifier lookup
+		        	if (model.getNsURI() == null) {
+						while (true) {
+							if (model.getESuperPackage() == null) {
+								break;
+							}
+							model = model.getESuperPackage();
+						}
+		        	}
+		        	
+		        	if(nextDesc.getLoadStatus().getSeverity() < IStatus.ERROR) {
+		        		metamodels.add(model);	        	
+		        		qvtEnv.getEPackageRegistry().put(model.getNsURI(), model);
+		        	}
+		            break;
+		        }
+            }
 		} catch (EmfException e) {
 			// It's legal situation of unresolved metamodels
 		}
