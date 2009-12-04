e6790f0ee5bf1f0b6e379c5c8b3b2aaf59d7ecee
radvorak <radvorak>
2009-12-04 20:17:32 +0000

    [296633] Provide debug.ui implementation

---
+++ plugins/org.eclipse.m2m.qvt.oml.debug.ui/src/org/eclipse/m2m/internal/qvt/oml/debug/ui/DebugUIMessages.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Eclipse Modeling Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Radek Dvorak - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.m2m.internal.qvt.oml.debug.ui;
+
+import org.eclipse.osgi.util.NLS;
+
+public class DebugUIMessages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipse.m2m.internal.qvt.oml.debug.ui.DebugUIMessages"; //$NON-NLS-1$
+
+	public static String Activator_StatusDialog_Error;
+    public static String Activator_StatusDialog_Warning;
+    public static String Activator_StatusDialog_Information;
+
+    public static String BreakpointConditionEditor_EnterCondition;
+    public static String EnableDisableBreakpointRulerAction_EnableBreakpoint;
+	public static String EnableDisableBreakpointRulerAction_DisableBreakpoint;
+	public static String EnableDisableBreakpointRulerAction_EnableDisableBreakpoint;
+	public static String EnableDisableBreakpointRulerAction_ErrorTitle;
+	public static String EnableDisableBreakpointRulerAction_ErrorMessage;
+	public static String ManageBreakpointRulerAction_text;	
+	public static String QvtBreakpointListener_CompilationFailed;
+
+    public static String QvtBreakpointListener_ExceptionInRuntime;
+
+    public static String QvtBreakpointPage_CancelBreakpointCreationError;
+
+    public static String QvtBreakpointPage_CreateBreakpointForModule;
+
+    public static String QvtBreakpointPage_Enabled;
+
+    public static String QvtBreakpointPage_HitCount;
+
+    public static String QvtBreakpointPage_HitCountErrorMessage;
+
+    public static String QvtBreakpointPage_Module;
+
+    public static String QvtBreakpointPage_ModulePrepareBreakpoint;
+
+    public static String QVTOToggleBreakpointAdapter_VerifyBreakpointJob;
+	public static String QVTOToggleBreakpointAdapter_CannotSetBreakpoint;
+	public static String QvtLineBreakpointPage_ConditionTrue;
+
+    public static String QvtLineBreakpointPage_ConditionValueChange;
+
+    public static String QvtLineBreakpointPage_EnableCondition;
+//    public static String QvtLineBreakpointPage_EnableConditionWithCodeAssist;
+
+//    public static String QvtLineBreakpointPage_EnableConditionWithoutCodeAssist;
+
+    public static String QvtLineBreakpointPage_LineBreakpoint;
+
+    public static String QvtLineBreakpointPage_LineNumber;
+
+    public static String QvtLineBreakpointPage_SuspendCondition;
+
+    public static String QvtRunToLineAdapter_InvalidLocation;
+	public static String QvtRunToLineAdapter_NoFile;
+	public static String QvtRunToLineAdapter_NoDocument;
+	public static String QvtRunToLineAdapter_NoInput;
+    public static String BreakpointCondErrorHandler_EditConditionButtonLabel;
+    public static String BreakpointCondErrorHandler_DialogTitle;
+	
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, DebugUIMessages.class);
+	}
+
+	private DebugUIMessages() {
+	}
+}
---
+++ plugins/org.eclipse.m2m.qvt.oml.debug.ui/src/org/eclipse/m2m/internal/qvt/oml/debug/ui/launch/QVTOApplicationConfigurationTabGroup.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Eclipse Modeling Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Radek Dvorak - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.m2m.internal.qvt.oml.debug.ui.launch;
+
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.CommonTab;
+import org.eclipse.debug.ui.EnvironmentTab;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaArgumentsTab;
+import org.eclipse.m2m.internal.qvt.oml.common.MdaException;
+import org.eclipse.m2m.internal.qvt.oml.emf.util.EmfUtil;
+import org.eclipse.m2m.internal.qvt.oml.runtime.project.ITransformationMaker;
+import org.eclipse.m2m.internal.qvt.oml.runtime.project.QvtInterpretedTransformation;
+import org.eclipse.m2m.internal.qvt.oml.runtime.project.QvtModule;
+import org.eclipse.m2m.internal.qvt.oml.runtime.project.QvtTransformation;
+import org.eclipse.m2m.internal.qvt.oml.runtime.project.TransformationUtil;
+import org.eclipse.m2m.internal.qvt.oml.runtime.ui.launch.QvtLauncherTab;
+import org.eclipse.m2m.internal.qvt.oml.runtime.ui.launch.QvtTransformationConfigurationTab;
+import org.eclipse.pde.ui.launcher.ConfigurationTab;
+import org.eclipse.pde.ui.launcher.PluginsTab;
+import org.eclipse.pde.ui.launcher.TracingTab;
+import org.eclipse.swt.custom.BusyIndicator;
+import org.eclipse.swt.widgets.Display;
+
+
+public class QVTOApplicationConfigurationTabGroup extends AbstractLaunchConfigurationTabGroup {
+	
+	public QVTOApplicationConfigurationTabGroup() {
+    	super();
+	}
+
+	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
+		// FIXME - eliminate this crazy thing 
+        ITransformationMaker transformation = new ITransformationMaker() {
+			public QvtTransformation makeTransformation(String name) throws MdaException {
+				QvtModule qvtModule = TransformationUtil.getQvtModule(EmfUtil.makeUri(name));
+		        return new QvtInterpretedTransformation(qvtModule);
+			}
+		};
+		
+		ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] {
+                new QvtLauncherTab(transformation, new ResourceSetImpl()),
+                new QvtTransformationConfigurationTab(transformation),
+                new QVTOMainTab(),
+                new JavaArgumentsTab(), 
+                new PluginsTab(), 
+                new ConfigurationTab(), 
+                new TracingTab(), 
+                new EnvironmentTab(), 
+                new CommonTab()
+        };
+        
+		setTabs(tabs);
+	}
+	
+	@Override
+	public void initializeFrom(ILaunchConfiguration configuration) {
+		final ILaunchConfiguration config = configuration;
+		final ILaunchConfigurationTab[] tabs = getTabs();
+		BusyIndicator.showWhile(Display.getCurrent(), new Runnable() {
+			public void run() {
+				for (int i = 0; i < tabs.length; i++) {
+					tabs[i].initializeFrom(config);
+				}
+			}
+		});
+	}
+}
