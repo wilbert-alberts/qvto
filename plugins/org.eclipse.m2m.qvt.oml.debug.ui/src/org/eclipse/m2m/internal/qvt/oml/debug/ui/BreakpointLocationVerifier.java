552859376c3318dbca85ebe0b9a696cee1de27ba
radvorak <radvorak>
2009-12-07 10:20:40 +0000

    - fix breapoint rejected

--- plugins/org.eclipse.m2m.qvt.oml.debug.ui/src/org/eclipse/m2m/internal/qvt/oml/debug/ui/BreakpointLocationVerifier.java
+++ plugins/org.eclipse.m2m.qvt.oml.debug.ui/src/org/eclipse/m2m/internal/qvt/oml/debug/ui/BreakpointLocationVerifier.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.model.ILineBreakpoint;
 import org.eclipse.jface.text.BadLocationException;
@@ -119,7 +120,7 @@ class BreakpointLocationVerifier {
             return canceled();
 		}
 
-		return canceled();
+		return Status.OK_STATUS;
 	}
 		
 	/**
