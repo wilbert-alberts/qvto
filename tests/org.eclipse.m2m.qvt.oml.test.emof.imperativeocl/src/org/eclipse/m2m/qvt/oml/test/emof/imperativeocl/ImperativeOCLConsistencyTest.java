4a0df1af4ccb790cdcd3b41c748a5213da9aa81d
radvorak <radvorak>
2009-12-09 14:02:59 +0000

    [295316] Ed.Willink - Update QVT models to QVT 1.1, OCL 2.1

--- tests/org.eclipse.m2m.qvt.oml.test.emof.imperativeocl/src/org/eclipse/m2m/qvt/oml/test/emof/imperativeocl/ImperativeOCLConsistencyTest.java
+++ tests/org.eclipse.m2m.qvt.oml.test.emof.imperativeocl/src/org/eclipse/m2m/qvt/oml/test/emof/imperativeocl/ImperativeOCLConsistencyTest.java
@@ -32,7 +32,7 @@ import org.eclipse.qvt.declarative.test.emof.tools.EquivalenceMap;
 
 public class ImperativeOCLConsistencyTest extends AbstractEssentialOCLConsistencyTest
 {	
-	public static void expectedDifferences(EquivalenceHelper helper, Set<EcoreDifference> differences) {
+	public static void expectedEcoreDifferences(EquivalenceHelper helper, Set<EcoreDifference> differences, ResourceSet rightResourceSet) {
 		changeOfURI(differences, helper, org.eclipse.m2m.qvt.oml.ecore.ImperativeOCL.ImperativeOCLPackage.eINSTANCE);
 		changeOfPrefix(differences, helper, org.eclipse.m2m.qvt.oml.ecore.ImperativeOCL.ImperativeOCLPackage.eINSTANCE);			
 		//
@@ -47,25 +47,36 @@ public class ImperativeOCLConsistencyTest extends AbstractEssentialOCLConsistenc
 		missingConstraint(differences, helper, org.eclipse.m2m.qvt.oml.ecore.ImperativeOCL.ImperativeOCLPackage.Literals.ORDERED_TUPLE_TYPE);
 	}
 
-	public static void expectedEmofDifferences(Set<EcoreDifference> differences, EquivalenceHelper helper) {
+	public static void expectedEmofDifferences(Set<EcoreDifference> differences, EquivalenceHelper helper, ResourceSet rightResourceSet) {
 		changeOfURI(differences, helper, ImperativeOCLPackage.eINSTANCE);
 		changeOfPrefix(differences, helper, ImperativeOCLPackage.eINSTANCE);
 		changeOfName(differences, helper, ImperativeOCLPackage.eINSTANCE);
+
+		EPackage rightImperativeOCL = getPackage(rightResourceSet, IMPERATIVE_OCL_PACKAGE_NAME.toLowerCase());
+		extraObject(differences, helper, getClass(rightImperativeOCL, "TemplateParameterType"));
+//		extraObject(differences, helper, getFeature(getClass(rightPackage, "TemplateParameterType"), "specification"));
+		missingClass(differences, helper, ImperativeOCLPackage.Literals.LIST_LITERAL_EXP);
+//		missingObject(differences, helper, ImperativeOCLPackage.Literals.LIST_LITERAL_EXP__ELEMENT);
+//		missingOppositeAnnotation(differences, helper, ImperativeOCLPackage.Literals.LIST_LITERAL_EXP__ELEMENT);
+		changeOfListObject(differences, helper, ImperativeOCLPackage.eINSTANCE, EcorePackage.Literals.EPACKAGE__ECLASSIFIERS, ImperativeOCLPackage.Literals.LIST_LITERAL_EXP, getClass(rightImperativeOCL, "TemplateParameterType"));		
 	}
 
-	public static void expectedEmof2OmgEmofDifferences(Set<EcoreDifference> differences, EquivalenceHelper helper) {
+	public static void expectedEmof2OmgEmofDifferences(Set<EcoreDifference> differences, EquivalenceHelper helper, ResourceSet rightResourceSet) {
 		missingURI(differences, helper, ImperativeOCLPackage.eINSTANCE);
 		missingPrefix(differences, helper, ImperativeOCLPackage.eINSTANCE);
 		changeOfName(differences, helper, ImperativeOCLPackage.eINSTANCE);
+		EPackage rightImperativeOCL = getPackage(rightResourceSet, IMPERATIVE_OCL_PACKAGE_NAME.toLowerCase());
+		changeOfListObject(differences, helper, ImperativeOCLPackage.eINSTANCE, EcorePackage.Literals.EPACKAGE__ECLASSIFIERS, ImperativeOCLPackage.Literals.LIST_LITERAL_EXP, getClass(rightImperativeOCL, "TemplateParameterType"));		
 	}
 
 	@Override
 	protected Set<EcoreDifference> getExpectedEcore2EmofDifferences(EquivalenceHelper helper, EPackage rightPackage) {
+		ResourceSet rightResourceSet = rightPackage.eResource().getResourceSet();
 		Set<EcoreDifference> differences = new HashSet<EcoreDifference>();
-		ImperativeOCLConsistencyTest.expectedDifferences(helper, differences);		
+		ImperativeOCLConsistencyTest.expectedEcoreDifferences(helper, differences, rightResourceSet);		
 		helper.setLogStream(null);
-		EssentialOCLConsistencyTest.expectedEcoreDifferences(differences, helper);
-		EMOFConsistencyTest.expectedEcore2RoseDifferences(helper, differences, rightPackage.eResource().getResourceSet());		
+		EssentialOCLConsistencyTest.expectedEcoreDifferences(differences, helper, rightResourceSet);
+		EMOFConsistencyTest.expectedEcore2RoseDifferences(helper, differences, rightResourceSet);		
 		return differences;
 	}
 
@@ -81,6 +92,14 @@ public class ImperativeOCLConsistencyTest extends AbstractEssentialOCLConsistenc
 		changeOfLower(differences, helper, ImperativeOCLPackage.Literals.RETURN_EXP__VALUE);
 		changeOfDefaultValue(differences, helper, ImperativeOCLPackage.Literals.ASSERT_EXP__SEVERITY);
 		changeOfDefaultValue(differences, helper, ImperativeOCLPackage.Literals.VARIABLE_INIT_EXP__WITH_RESULT);
+
+		extraObject(differences, helper, getClass(rightPackage, "TemplateParameterType"));
+		extraObject(differences, helper, getFeature(getClass(rightPackage, "TemplateParameterType"), "specification"));
+		missingClass(differences, helper, ImperativeOCLPackage.Literals.LIST_LITERAL_EXP);
+		missingObject(differences, helper, ImperativeOCLPackage.Literals.LIST_LITERAL_EXP__ELEMENT);
+		missingOppositeAnnotation(differences, helper, ImperativeOCLPackage.Literals.LIST_LITERAL_EXP__ELEMENT);
+		changeOfListObject(differences, helper, ImperativeOCLPackage.eINSTANCE, EcorePackage.Literals.EPACKAGE__ECLASSIFIERS, ImperativeOCLPackage.Literals.LIST_LITERAL_EXP, getClass(rightPackage, "TemplateParameterType"));		
+
 		changeOfList(differences, helper, ImperativeOCLPackage.Literals.SWITCH_EXP, EcorePackage.Literals.ECLASS__ESUPER_TYPES);
 		missingOpposite(differences, helper, ImperativeOCLPackage.Literals.ALT_EXP__BODY);
 		missingOpposite(differences, helper, ImperativeOCLPackage.Literals.ALT_EXP__CONDITION);
@@ -114,8 +133,9 @@ public class ImperativeOCLConsistencyTest extends AbstractEssentialOCLConsistenc
 		missingOpposite(differences, helper, ImperativeOCLPackage.Literals.WHILE_EXP__BODY);
 		missingOpposite(differences, helper, ImperativeOCLPackage.Literals.WHILE_EXP__CONDITION);
 		helper.setLogStream(null);
-		EMOFConsistencyTest.expectedEmofDifferences(differences, helper);		
-		EssentialOCLConsistencyTest.expectedEmofDifferences(differences, helper);
+		ResourceSet rightResourceSet = rightPackage.eResource().getResourceSet();
+		EMOFConsistencyTest.expectedEmofDifferences(differences, helper, rightResourceSet);		
+		EssentialOCLConsistencyTest.expectedEmofDifferences(differences, helper, rightResourceSet);
 		return differences;
 	}
 
@@ -131,6 +151,13 @@ public class ImperativeOCLConsistencyTest extends AbstractEssentialOCLConsistenc
 		changeOfDefaultValue(differences, helper, ImperativeOCLPackage.Literals.ASSERT_EXP__SEVERITY);
 		changeOfDefaultValue(differences, helper, ImperativeOCLPackage.Literals.VARIABLE_INIT_EXP__WITH_RESULT);
 		changeOfList(differences, helper, ImperativeOCLPackage.Literals.SWITCH_EXP, EcorePackage.Literals.ECLASS__ESUPER_TYPES);
+
+		extraObject(differences, helper, getClass(rightPackage, "TemplateParameterType"));
+		extraObject(differences, helper, getFeature(getClass(rightPackage, "TemplateParameterType"), "specification"));
+		missingClass(differences, helper, ImperativeOCLPackage.Literals.LIST_LITERAL_EXP);
+		missingObject(differences, helper, ImperativeOCLPackage.Literals.LIST_LITERAL_EXP__ELEMENT);
+		missingOppositeAnnotation(differences, helper, ImperativeOCLPackage.Literals.LIST_LITERAL_EXP__ELEMENT);
+		changeOfListObject(differences, helper, ImperativeOCLPackage.eINSTANCE, EcorePackage.Literals.EPACKAGE__ECLASSIFIERS, ImperativeOCLPackage.Literals.LIST_LITERAL_EXP, getClass(rightPackage, "TemplateParameterType"));		
 
 		changeOfSize(differences, helper, ImperativeOCLPackage.Literals.SEVERITY_KIND, EcorePackage.Literals.EENUM__ELITERALS);
 		missingEnumerationLiteral(differences, helper, ImperativeOCLPackage.Literals.SEVERITY_KIND.getEEnumLiteral("error"));
@@ -157,7 +184,7 @@ public class ImperativeOCLConsistencyTest extends AbstractEssentialOCLConsistenc
 		changeOfLower(differences, helper, ImperativeOCLPackage.Literals.RAISE_EXP__ARGUMENT);
 		changeOfLower(differences, helper, ImperativeOCLPackage.Literals.SWITCH_EXP__ALTERNATIVE_PART);
 		changeOfLower(differences, helper, ImperativeOCLPackage.Literals.SWITCH_EXP__ELSE_PART);
-		changeOfLower(differences, helper, ImperativeOCLPackage.Literals.TEMPLATE_PARAMETER_TYPE__SPECIFICATION);
+//		changeOfLower(differences, helper, ImperativeOCLPackage.Literals.TEMPLATE_PARAMETER_TYPE__SPECIFICATION);
 		changeOfLower(differences, helper, ImperativeOCLPackage.Literals.TRY_EXP__EXCEPT_CLAUSE);
 		changeOfLower(differences, helper, ImperativeOCLPackage.Literals.TRY_EXP__TRY_BODY);
 		changeOfLower(differences, helper, ImperativeOCLPackage.Literals.TYPEDEF__CONDITION);
@@ -217,10 +244,19 @@ public class ImperativeOCLConsistencyTest extends AbstractEssentialOCLConsistenc
 		changeOfLower(differences, helper, ImperativeOCLPackage.Literals.RETURN_EXP__VALUE);
 		changeOfDefaultValue(differences, helper, ImperativeOCLPackage.Literals.ASSERT_EXP__SEVERITY);
 		changeOfDefaultValue(differences, helper, ImperativeOCLPackage.Literals.VARIABLE_INIT_EXP__WITH_RESULT);
+
+		extraObject(differences, helper, getClass(rightPackage, "TemplateParameterType"));
+		extraObject(differences, helper, getFeature(getClass(rightPackage, "TemplateParameterType"), "specification"));
+		missingClass(differences, helper, ImperativeOCLPackage.Literals.LIST_LITERAL_EXP);
+		missingObject(differences, helper, ImperativeOCLPackage.Literals.LIST_LITERAL_EXP__ELEMENT);
+		missingOppositeAnnotation(differences, helper, ImperativeOCLPackage.Literals.LIST_LITERAL_EXP__ELEMENT);
+		changeOfListObject(differences, helper, ImperativeOCLPackage.eINSTANCE, EcorePackage.Literals.EPACKAGE__ECLASSIFIERS, ImperativeOCLPackage.Literals.LIST_LITERAL_EXP, getClass(rightPackage, "TemplateParameterType"));		
+
 		extraListObject(differences, helper, ImperativeOCLPackage.Literals.SWITCH_EXP, EcorePackage.Literals.ECLASS__ESUPER_TYPES, getClass(rightPackage, "SwitchExp").getESuperTypes().get(0));		
 		helper.setLogStream(null);
-		EMOFConsistencyTest.expectedEmofDifferences(differences, helper);		
-		EssentialOCLConsistencyTest.expectedEmofDifferences(differences, helper);
+		ResourceSet rightResourceSet = rightPackage.eResource().getResourceSet();
+		EMOFConsistencyTest.expectedEmofDifferences(differences, helper, rightResourceSet);		
+		EssentialOCLConsistencyTest.expectedEmofDifferences(differences, helper, rightResourceSet);
 		return differences;
 	}
 
@@ -261,13 +297,19 @@ public class ImperativeOCLConsistencyTest extends AbstractEssentialOCLConsistenc
 	@Override
 	protected void installEmofOmgMappings(EquivalenceMap comparator, ResourceSet resourceSet) {
 		super.installEmofOmgMappings(comparator, resourceSet);
-		comparator.putEquivalence(ImperativeOCLPackage.eINSTANCE, getPackage(resourceSet, IMPERATIVE_OCL_PACKAGE_NAME.toLowerCase()));
+		EPackage emofPackage = getPackage(resourceSet, IMPERATIVE_OCL_PACKAGE_NAME.toLowerCase());
+		comparator.putEquivalence(ImperativeOCLPackage.eINSTANCE, emofPackage);
+		comparator.putEquivalence(ImperativeOCLPackage.Literals.LIST_LITERAL_EXP, null);
+		comparator.putEquivalence(null, getClass(emofPackage, "TemplateParameterType"));
 	}
 
 	@Override
 	protected void installEmof2EmofMappings(EquivalenceMap comparator, ResourceSet resourceSet) {
 		super.installEmof2EmofMappings(comparator, resourceSet);
-		comparator.putEquivalence(ImperativeOCLPackage.eINSTANCE, getPackage(resourceSet, IMPERATIVE_OCL_PACKAGE_NAME.toLowerCase()));
+		EPackage emofPackage = getPackage(resourceSet, IMPERATIVE_OCL_PACKAGE_NAME.toLowerCase());
+		comparator.putEquivalence(ImperativeOCLPackage.eINSTANCE, emofPackage);
+		comparator.putEquivalence(ImperativeOCLPackage.Literals.LIST_LITERAL_EXP, null);
+		comparator.putEquivalence(null, getClass(emofPackage, "TemplateParameterType"));
 	}
 	
 	public void testImperativeOCLConsistency() throws IOException, MappingConfigurationException {
@@ -277,10 +319,10 @@ public class ImperativeOCLConsistencyTest extends AbstractEssentialOCLConsistenc
 		getExpectedDerivedPackageInconsistencies(ecoreInconsistencies, adaptingInconsistencies);
 		htmlStream.setEnabled(true);
 		ecore2EmofAdapterConsistencyTest(ecoreInconsistencies, adaptingInconsistencies);
-//		emof2OmgRoseConsistencyTest(ImperativeOCLPackage.eNS_URI, "imperativeocl.ecore");
-//		emof2OmgEcoreConsistencyTest(ImperativeOCLPackage.eNS_URI, "imperativeocl.ecore");
-//		emof2OmgEmofConsistencyTest(ImperativeOCLPackage.eNS_URI);
-//		emof2RoseConsistencyTest(ImperativeOCLPackage.eNS_URI, "ImperativeOCL.ecore");
+		emof2OmgRoseConsistencyTest(ImperativeOCLPackage.eNS_URI, "imperativeocl.ecore");
+		emof2OmgEcoreConsistencyTest(ImperativeOCLPackage.eNS_URI, "imperativeocl.ecore");
+		emof2OmgEmofConsistencyTest(ImperativeOCLPackage.eNS_URI);
+		emof2RoseConsistencyTest(ImperativeOCLPackage.eNS_URI, "ImperativeOCL.ecore");
 		ecore2EmofConsistencyTest(org.eclipse.m2m.qvt.oml.ecore.ImperativeOCL.ImperativeOCLPackage.eNS_URI,
 				ImperativeOCLPackage.eINSTANCE,
 				org.eclipse.qvt.declarative.emof.EssentialOCL.EssentialOCLPackage.eINSTANCE,
