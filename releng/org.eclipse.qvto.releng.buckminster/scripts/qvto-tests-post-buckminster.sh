4129742f8c7c4467002ab69a20534a9ed06d270b
Ed.Willink <ed@willink.me.uk>
2012-06-20 06:33:08 +0100

    [382990] Initial attempt at a Buckminster build

---
+++ releng/org.eclipse.qvto.releng.buckminster/scripts/qvto-tests-post-buckminster.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mv buildroot/buckminster.output/org.eclipse.qvto.releng.build_*-eclipse.feature/site.p2 MMT-QVTo.p2.repository
+mv buildroot/buckminster.output/org.eclipse.qvto.releng.build_*-eclipse.feature/zips MMT-QVTo.downloads
