761946f989b44b5ded8035e4cbab2571fe592dd7
Ed Willink <ed@willink.me.uk>
2013-03-18 18:51:06 +0000

    [releng] Accommodate unspecified MANAGE_JAVADOC

--- releng/org.eclipse.qvto.releng.buckminster/scripts/qvto-tests-post-buckminster.sh
+++ releng/org.eclipse.qvto.releng.buckminster/scripts/qvto-tests-post-buckminster.sh
@@ -13,7 +13,7 @@
 mv buildroot/buckminster.output/org.eclipse.qvto.releng.build_*-eclipse.feature/site.p2 MMT-QVTo.p2.repository
 mv buildroot/buckminster.output/org.eclipse.qvto.releng.build_*-eclipse.feature/zips MMT-QVTo.downloads
 
-if [ ${MANAGE_JAVADOC} = "true" ]
+if [ ${MANAGE_JAVADOC:="false"} = "true" ]
 then
   mkdir MMT-QVTo.javadoc
   mv buildroot/buckminster.output/org.eclipse.qvto.releng.buckminster_*-buckminster/javadoc/MMT-QVTo-javadoc.zip MMT-QVTo.javadoc/MMT-QVTo-javadoc.zip
