c06bc99db9b840e789b41caa519e8121ae896471
Adolfo SBH <adolfosbh@gmail.com>
2013-04-09 17:46:44 +0100

    [397429] - Use Xtext and align QVTo with the OCL Pivot model - Changes to generated visitors after regenerating using the new infrastructure

--- examples/org.eclipse.qvto.examples.pivot.qvtoperational/emf-gen/org/eclipse/qvto/examples/pivot/qvtoperational/util/QVTOperationalVisitor.java
+++ examples/org.eclipse.qvto.examples.pivot.qvtoperational/emf-gen/org/eclipse/qvto/examples/pivot/qvtoperational/util/QVTOperationalVisitor.java
@@ -1,20 +1,18 @@
 /**
  * <copyright>
- *
- * Copyright (c) 2010, 2013 E.D.Willink, University of York and others.
+ * Copyright (c) 2013 Willink Transformations, University of York, and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *     E.D.Willink - initial API and implementation
- *     Adolfo Sanchez-Barbudo Herrera (University of York) - bug397429
- *
+ *   E.D.Willink - Initial API and implementation
+ *   Adolfo Sanchez-Barbudo (University of York) - Bug397429
  * </copyright>
  *
  * This code is auto-generated
- * from: /org.eclipse.qvto.examples.pivot.qvtoperational/model/QVTOperational.ecore
+ * from: /org.eclipse.qvto.examples.pivot.qvtoperational/model/QVTOperational.genmodel
  * by: org.eclipse.ocl.examples.build.acceleo.GenerateVisitor
  * defined by: org.eclipse.ocl.examples.build.acceleo.generateVisitors.mtl
  * invoked by: org.eclipse.ocl.examples.build.utilities.*
