94367c6586c757bbba3129b15f3352c891cc5e3b
Adolfo SBH <adolfosbh@gmail.com>
2013-07-25 18:40:38 +0100

    [397429] - Some minor test case variations
    - https://bugs.eclipse.org/bugs/show_bug.cgi?id=397429

--- examples/org.eclipse.qvto.examples.xtext.qvtoperational.tests/src/org/eclipse/qvto/examples/xtext/qvtoperational/tests/QVToXtextTestCase.java
+++ examples/org.eclipse.qvto.examples.xtext.qvtoperational.tests/src/org/eclipse/qvto/examples/xtext/qvtoperational/tests/QVToXtextTestCase.java
@@ -4,18 +4,53 @@ package org.eclipse.qvto.examples.xtext.qvtoperational.tests;
 public class QVToXtextTestCase extends AbstractQVToXtextTestCase {
 
 	
-	
-	public void testSimpleLoadAsString() throws Exception {
-				
+	public void testOCLInEcoreLoadAsString() throws Exception {
+		
 		String testFile =			
-			"metamodel mm1 {\n" +
-			"	class c1 {\n" +
-			"		prop1 : String ;\n"+	
-			"	};\n" +
-			"	class c2 extends c1 {\n"+
-			"		prop2 : c1;\n"+
-			"	};\n"+
-			"}";
+			"package mm1 : mm1 = 'mm1' {"				
+			+	"class c1	{\n"
+			+	"	attribute prop1 : String;\n" 
+			+	"}\n" 
+			+	"class c2 extends c1 {\n"
+			+	"	property prop2 : c1;\n"
+			+	"}\n"
+			+"}";
+		doLoadFromString("loadAsString.oclinecore", testFile);
+	}
+	
+	public void testQVToLoadAsString() throws Exception {
+				
+		String testFile =
+			"metamodel mm1 {\n"
+			+	"class c1 {\n"
+			+	"	prop1 : String ;\n"	
+			+	"};\n" 
+			+	"class c2 extends c1 {\n"
+			+	"	prop2 : c1;\n"
+			+	"};\n"
+			+"}";
 		doLoadFromString("loadAsString.qvto", testFile);
+	}
+	
+	public void testSimpleMetamodelDefinition() throws Exception {
+		String testFile =
+				"metamodel mm1 {\n"
+				+ "	datatype myDataType;" 
+				+ "	class c1 {\n"
+				+ "		prop1 : String ;\n"
+				+ "	};"
+				+ "}";
+			doLoadFromString("simpleMetamodelDefinition.qvto", testFile);
+	}
+	
+	public void testSimpleMetamodelDefinitionWithoutOptionalSemicolon() throws Exception {
+		String testFile =
+				"metamodel mm1 {\n"
+				+ "	datatype myDataType" 
+				+ "	class c1 {\n"
+				+ "		prop1 : String ;\n"
+				+ "	}"
+				+ "}";
+			doLoadFromString("simpleMetamodelDefinitionWithouOptionalSemicolon.qvto", testFile);
 	}
 }
