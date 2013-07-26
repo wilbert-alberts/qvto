b86ebd928f22e124e31bf1f25244d2d00b5ace83
Adolfo SBH <adolfosbh@gmail.com>
2013-07-26 12:44:18 +0100

    [397429] - ImperativeOCLMetamodelManager
    - https://bugs.eclipse.org/bugs/show_bug.cgi?id=397429

--- examples/org.eclipse.qvto.examples.xtext.imperativeocl/src/org/eclipse/qvto/examples/xtext/imperativeocl/utilities/ImperativeOCLCSResource.java
+++ examples/org.eclipse.qvto.examples.xtext.imperativeocl/src/org/eclipse/qvto/examples/xtext/imperativeocl/utilities/ImperativeOCLCSResource.java
@@ -3,10 +3,13 @@ package org.eclipse.qvto.examples.xtext.imperativeocl.utilities;
 import java.util.Map;
 
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.ocl.examples.pivot.manager.MetaModelManager;
+import org.eclipse.ocl.examples.pivot.manager.MetaModelManagerResourceSetAdapter;
 import org.eclipse.ocl.examples.xtext.base.cs2as.CS2Pivot;
 import org.eclipse.ocl.examples.xtext.essentialocl.utilities.EssentialOCLCSResource;
+import org.eclipse.qvto.examples.pivot.imperativeocl.manager.ImperativeOCLMetamodelManager;
 import org.eclipse.qvto.examples.xtext.imperativeocl.cs2as.ImperativeOCLCS2AS;
 
 public class ImperativeOCLCSResource extends EssentialOCLCSResource {
@@ -22,5 +25,25 @@ public class ImperativeOCLCSResource extends EssentialOCLCSResource {
 	@Override
 	public @NonNull String getEditorName() {
 		return "Imperative OCL";
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ocl.examples.xtext.essentialocl.utilities.EssentialOCLCSResource#createMetaModelManager()
+	 */
+	@Override
+	public final @NonNull MetaModelManager createMetaModelManager() {
+		// FIXME move this to EssentialOCLCS Resource		
+		ResourceSet resourceSet = getResourceSet();
+		if (resourceSet != null) {
+			MetaModelManagerResourceSetAdapter resourceSetAdapter = MetaModelManagerResourceSetAdapter.findAdapter(resourceSet);
+			if (resourceSetAdapter != null) {
+				return resourceSetAdapter.getMetaModelManager();
+			}
+		}
+		return doCreateMetamodeManager();
+	}
+
+	protected MetaModelManager doCreateMetamodeManager() {
+		return new ImperativeOCLMetamodelManager();
 	}
 }
