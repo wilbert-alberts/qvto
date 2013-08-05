d4dee8b6519ba600db04bfe888214b10b29a5b23
sboyko <serg.boyko2011@gmail.com>
2013-08-05 14:15:07 +0400

    [414407] - QVT editor: add Quick Outline support (Ctrl + O)

--- plugins/org.eclipse.m2m.qvt.oml.editor.ui/src/org/eclipse/m2m/internal/qvt/oml/editor/ui/actions/OpenDeclarationAction.java
+++ plugins/org.eclipse.m2m.qvt.oml.editor.ui/src/org/eclipse/m2m/internal/qvt/oml/editor/ui/actions/OpenDeclarationAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2009 Borland Software Corporation and others.
+ * Copyright (c) 2007, 2013 Borland Software Corporation and others.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -29,13 +29,6 @@ public class OpenDeclarationAction extends TextEditorAction {
 	
 	public static final String OPEN_DECLARATION = "org.eclipse.m2m.qvt.oml.editor.ui.actions.OpenDeclaration"; //$NON-NLS-1$
 	
-	public OpenDeclarationAction(ResourceBundle bundle, String prefix, ITextEditor editor, int style) {
-		super(bundle, prefix, editor, style);
-		
-		assert editor instanceof QvtEditor;
-		setEnabled(true);
-	}
-
 	public OpenDeclarationAction(ResourceBundle bundle, String prefix, ITextEditor editor) {
 		super(bundle, prefix, editor);
 		setEnabled(true);
