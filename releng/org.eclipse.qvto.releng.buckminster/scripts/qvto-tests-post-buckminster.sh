c9679595a2f377c094c0607c32ad8eec8b653d09
sboyko <serg.boyko2011@gmail.com>
2013-08-21 13:48:18 +0400

    [releng] Configuration updates for Luna

--- releng/org.eclipse.qvto.releng.buckminster/scripts/qvto-tests-post-buckminster.sh
+++ releng/org.eclipse.qvto.releng.buckminster/scripts/qvto-tests-post-buckminster.sh
@@ -10,12 +10,12 @@
 #*******************************************************************************
 #!/bin/bash
 
-mv buildroot/buckminster.output/org.eclipse.qvto.releng.build_*-eclipse.feature/site.p2 QVTo.p2.repository
-mv buildroot/buckminster.output/org.eclipse.qvto.releng.build_*-eclipse.feature/zips QVTo.downloads
+mv buildroot/buckminster.output/org.eclipse.qvto.releng.build_*-eclipse.feature/site.p2 MMT-QVTo.p2.repository
+mv buildroot/buckminster.output/org.eclipse.qvto.releng.build_*-eclipse.feature/zips MMT-QVTo.downloads
 
 if [ ${MANAGE_JAVADOC:="false"} = "true" ]
 then
-  mkdir QVTo.javadoc
-  mv buildroot/buckminster.output/org.eclipse.qvto.releng.buckminster_*-buckminster/javadoc/QVTo-javadoc.zip QVTo.javadoc/QVTo-javadoc.zip
+  mkdir MMT-QVTo.javadoc
+  mv buildroot/buckminster.output/org.eclipse.qvto.releng.buckminster_*-buckminster/javadoc/MMT-QVTo-javadoc.zip MMT-QVTo.javadoc/MMT-QVTo-javadoc.zip
   rm -rf buildroot/buckminster.output/org.eclipse.qvto.releng.buckminster_*-buckminster/javadoc
 fi
