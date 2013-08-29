6fe0f873f974b74eafbaca715be2a5ac06e8fdd0
Adolfo SBH <adolfosbh@gmail.com>
2013-08-29 16:50:06 +0100

    [397429] - Uploading some doc
    - https://bugs.eclipse.org/bugs/show_bug.cgi?id=397429
    - Bug 397429

---
+++ examples/org.eclipse.qvto.examples.xtext.qvtoperational/src/org/eclipse/qvto/examples/xtext/qvtoperational/cs2as/attributions/MappingExtensionCSAttribution.java
@@ -0,0 +1,8 @@
+package org.eclipse.qvto.examples.xtext.qvtoperational.cs2as.attributions;
+
+import org.eclipse.ocl.examples.pivot.scoping.AbstractAttribution;
+
+
+public class MappingExtensionCSAttribution extends AbstractAttribution {
+
+}
